42aec402b4944e4d90f4b38f2a1e69186690ff62
Thibault <49438882+Thibault-Cart@users.noreply.github.com>
2023-05-22 10:40:50 +0200

    commentaire

--- codeM5Stack_Hall/main.py
+++ codeM5Stack_Hall/main.py
@@ -7,12 +7,15 @@ import wifiConnection
 
 #ip du m5stack "192.168.105.51"
 
+# machine d'état pour savoir quand valeur capteur change
 Anc_EtatCapteurHall=""
-PORT = 1234
-
-IP_PUBLISHER = "192.168.27.248"
 EtatCapteurHall=""
 
+# constantes pour comunication TCP
+PORT = 1234
+IP_PUBLISHER = "192.168.27.248"
+
+# class permantant la lecture du capteur HALL
 class Hall():
      def __init__(self):
          self.pin = Pin(22,Pin.IN)
@@ -29,27 +32,28 @@ class Hall():
              
 
 
-def lcdWrite(posX: int, posY: int, text: int):
-    lcd.clear()
-    lcd.text(posX, posY, text)
-
 def main():
     global Anc_EtatCapteurHall,EtatCapteurHall,IP_PUBLISHER,PORT
-    # connexion au wifi
+    
+    # appel la fonction connect du fichier wifiConnection.py
     wifiConnection.connect()
+    # instantiation Hall
     h=Hall()
     
     socketTcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
 
     print("connecter")
+    
     while(True):
-        
+        # s ichangement d'état du capteur
         if(Anc_EtatCapteurHall!=EtatCapteurHall):
+        #creation socket
             socketTcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #connection socket
             socketTcp.connect((IP_PUBLISHER, PORT))
             print(EtatCapteurHall)
-            
+            #envoie data au publisher
             socketTcp.send((EtatCapteurHall).encode("UTF-8"))
             # Recevoir la réponse du serveur
             response = socketTcp.recv(1024)  # Taille du tampon : 1024 octets
